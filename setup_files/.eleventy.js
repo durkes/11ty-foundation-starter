abfda187534a099fbbec7fe91d07cfdc313474bf
durkes <durkes@users.noreply.github.com>
2023-12-20 18:15:05 -0600

    Update config for js and disable sourcemaps

--- setup_files/.eleventy.js
+++ setup_files/.eleventy.js
@@ -6,14 +6,17 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css/**/*.css");
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addWatchTarget("./src/css/**/*.scss");
+    eleventyConfig.addWatchTarget("./src/js/**/*.js");
+
     eleventyConfig.on('beforeBuild', () => {
         execSync('npm run clean'); // delete last build to remove extra files
+        execSync('npx rollup -c'); // build javascript
 
         const result = sass.renderSync({ file: 'src/css/main.scss', outputStyle: 'compressed' });
         fs.mkdirSync('./_site/css', { recursive: true });
         fs.writeFileSync('./_site/css/main.css', result.css);
 
-        execSync('npx postcss ./_site/css/main.css --use autoprefixer -o ./_site/css/main.css');
+        execSync('npx postcss ./_site/css/main.css --use autoprefixer --no-map -o ./_site/css/main.css');
     });
 
     return {
